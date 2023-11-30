3ea4629d6979b6d0aaf9b0b77fe22cf400a49c3f
Brendon45 <jejebrendon722@gmail.com>
2023-11-30 05:09:39 -0800

    prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Author - Brendon Jeje
+
+if __name__ == '__main__':
+    import sys
+
+    #loop through values and add them
+    sum = 0
+    i = 1
+    while i < len(sys.argv):
+        sum += int(sys.argv[i])
+        i += 1
+
+    print('{}'.format(sum))
