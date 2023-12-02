e615ca7392d9efc1254ba67df25dc134af7f14c1
Brendon45 <jejebrendon722@gmail.com>
2023-12-02 12:58:45 -0800

    a function that replaces an element of a list at a specific position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    '''replace element at idx in my_list'''
+
+    '''if element is negative'''
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
