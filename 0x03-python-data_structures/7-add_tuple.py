3663c36282d2884ea1a86392cc3591b8ac85b921
Brendon45 <jejebrendon722@gmail.com>
2023-12-02 13:40:04 -0800

    tupple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    '''add tuples'''
+
+    '''if tuple is none'''
+    if len(tuple_a) < 2:
+        tuple_a += 0,
+        tuple_a += 0,
+    if len(tuple_b) < 2:
+        tuple_b += 0,
+        tuple_b += 0,
+    '''if tuple is 1'''
+    if len(tuple_a) < 2:
+        tuple_a += 0,
+    if len(tuple_b) < 2:
+        tuple_b += 0,
+
+    '''if tuple is more than 2'''
+    tup = ()
+    tup += tuple_a[0] + tuple_b[0],
+    tup += tuple_a[1] + tuple_b[1],
+
+    return (tup)
