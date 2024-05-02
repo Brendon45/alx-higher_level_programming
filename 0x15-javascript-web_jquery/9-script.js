2a555b6fc5925e7fd950c51c476c41edf44ea222
Brendon45 <jejebrendon722@gmail.com>
2024-05-02 22:20:40 +0200

    a script fetches from https://hellosalut.stefanbohacek.dev/\?lang\=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,14 +1,8 @@
-#!/usr/bin/node
-
-// Script fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and
-// displays the value of hello from that fetch in the HTML tag DIV#hello
+// Fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+// Displays the value of hello from that fetch in the HTML tag DIV#hello
 
 $(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    success: function (data) {
-      $('DIV#hello').append(data.hello);
-    }
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
   });
 });
